01756947ba8fb6f9583daf267c7e19e434d14821
Miguel <miguelgvizcaino@gmail.com>
2026-09-24 11:08:40 +0200

    fix: :bug: more urls

--- apps/frontend/lib/sockets.ts
+++ apps/frontend/lib/sockets.ts
@@ -1,5 +1,8 @@
+import 'dotenv/config';
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:4000', {
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL?.slice(0,22)
+
+export const socket = io(BACKEND_URL, {
     autoConnect: false, // no conecta hasta que lo digas explícitamente
 });
