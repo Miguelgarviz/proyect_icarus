bd4c462ecdda1af506ad88c146b80295ed42827f
Miguel <miguelgvizcaino@gmail.com>
2026-09-24 11:16:39 +0200

    feat: :beers: websockets url again

--- apps/frontend/lib/sockets.ts
+++ apps/frontend/lib/sockets.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { io } from 'socket.io-client';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL?.slice(0,22)
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL?.slice(0,43)
 
 export const socket = io(BACKEND_URL, {
     autoConnect: false, // no conecta hasta que lo digas explícitamente
